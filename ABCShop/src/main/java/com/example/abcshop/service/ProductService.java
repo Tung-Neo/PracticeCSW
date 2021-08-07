00e5c845de0ee4ef955b34de9dedf126e7006d5f
Tung-Neo <62163195+Tung-Neo@users.noreply.github.com>
2021-08-07 10:56:13 +0700

    PracticeCSW

---
+++ ABCShop/src/main/java/com/example/abcshop/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.example.abcshop.service;
+
+import com.example.abcshop.entity.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+	public List<Product> getProducts();
+
+    public void saveProduct(Product theProduct);
+
+	public Product getProduct(int theId);
+
+	public void deleteById(int theId);
+
+	public Product buy(int id, Product theProduct);
+
+}
